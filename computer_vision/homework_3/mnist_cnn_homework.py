a2a7a31db5af1e65e2acf72c0ee1f02c3d96230d
LogicStarDust <657496651@qq.com>
2017-09-07 00:26:17 +0800

    写作业

--- computer_vision/homework_3/mnist_cnn_homework.py
+++ computer_vision/homework_3/mnist_cnn_homework.py
@@ -1,4 +1,4 @@
-'''Trains a simple convnet on the MNIST dataset.
+"""Trains a simple convnet on the MNIST dataset.
 If implemented correctly, it is easy to get over 99.2% for testing accuracy
 after a few epochs. And thre is still a lot of margin for parameter tuning,
 e.g. different initializations, decreasing learning rates when the accuracy
@@ -8,16 +8,14 @@ stops increasing, or using model ensembling techniques, etc.
 经过几个epochs。 而对于参数调整来说，仍然有很大的余地，
 例如 不同的初始化，精度降低学习率
 停止增加，或使用模型组合技术等。
-'''
+"""
 
 from __future__ import print_function
-
-from keras import backend as K
+import keras
+from keras import backend as k
 from keras.datasets import mnist
 from keras.layers import Dense
 from keras.models import Sequential
-
-import util
 
 batch_size = 128
 num_classes = 10
@@ -30,10 +28,13 @@ img_rows, img_cols = 28, 28
 # the data, shuffled and split between train and test sets
 # 加载数据
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
-print("x_train:", x_train.shape, ",y_train:", y_train.shape)
-util.printImgFromMNIST(x_train, y_train, 1)
+print("训练集数据维度:", x_train.shape, ",其标签维度:", y_train.shape)
+# util.printImgFromMNIST(x_train, y_train, 1)
 
-if K.image_data_format() == 'channels_first':
+# 格式化数据。把训练集从（n,28,28）的形式变成（n,1,28,28）或者（n,28,28,1），其中1代表通道
+# 原始数据代表有n个数据，一个数据28行28列，每一个元素存放一个像素值
+# 转化数据代表有n个数据，一个数据38行38列，每一个又有一个数组，这里数组大小为1，放置一个像素值
+if k.image_data_format() == 'channels_first':
     x_train = x_train.reshape(x_train.shape[0], 1, img_rows, img_cols)
     x_test = x_test.reshape(x_test.shape[0], 1, img_rows, img_cols)
     input_shape = (1, img_rows, img_cols)
@@ -45,29 +46,31 @@ else:
 # In order to speed up the convergence, we may normalize the input values
 # so that they are in the range of (0, 1) for (-1, 1)
 # Your code here.
-# 为了快速收敛，我们可以标准化输入值。所以它们的范围在（0，1）从（-1，1）
-X_train = x_train
-util.printImgFromMNIST(X_train, y_train, 0)
-X_test = x_test
-
-print('x_train shape:', x_train.shape)
-print(x_train.shape[0], 'train samples')
-print(x_test.shape[0], 'test samples')
+# 为了快速收敛，我们可以标准化输入值。所以它们的范围从（0，1）到（-1，1）
+# TODO 试试减去0.5的效果
+X_train = x_train.astype("float32") / 255.0
+X_test = x_test.astype("float32") / 255.0
 
 # Convert class vectors to binary class matrices, e.g. "1" ==> [0,1,0,0,0,0,0,0,0,0]
 # 转换类向量成二进制矩阵，如"1" ==> [0,1,0,0,0,0,0,0,0,0]
-y_train = "Your code here"
-y_test = "Your code here"
+# 将类别向量(从0到nb_classes的整数向量)映射为二值类别矩阵, 用于应用到以categorical_crossentropy为目标函数的模型中.
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
 
+print("格式化后训练集数据维度:", x_train.shape, ",其标签维度:", y_train.shape)
+print(x_train.shape[0], '个训练数据')
+print(x_test.shape[0], '个测试数据')
+
+# 序贯模型是多个网络层的线性堆叠，可以通过.add()方法一个个的将layer加入模型
 model = Sequential()
 
 # Please build up a simple ConvNets by stacking a few conovlutioanl layers (kenel size with 3x3
 # is a good choice, don't foget using non-linear activations for convolutional layers),
 # max-pooling layers, dropout layers and dense/fully-connected layers.
-# 请建立简单的ConvNets通过堆积几个卷积层（核心大小为3x3）十个好的选择，不要忘记使用非线性激活给卷积层、
+# 请建立简单的ConvNets通过堆积几个卷积层（核心大小为3x3）是个好的选择，不要忘记使用非线性激活给卷积层、
 # 最大池化层，dropout层和dense/fully-connected 层.
 # Your code here.
-# model.add(...)
+model.add(Dense())
 # model.add(...)
 
 model.add(Dense(num_classes, activation='softmax'))
@@ -88,8 +91,7 @@ model.compile(loss="Your code here",
 # x_train = data_augment(...)
 
 
-
-# Extra Points 2: use K-Fold cross-validation for ensembling k models,
+# Extra Points 2: use K-Fold cross-validation for ensembling k models
 # i.e. (1) split the whole training data into K folds;
 #      (2) train K models based on different training data;
 #      (3) when evaludating the testing data, averaging over K model predictions as final output.
